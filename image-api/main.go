9f66ed37b09285ef0f9e641583344bc3262082f6
Hyunghwan Shin <hwan.shin@gmail.com>
2024-11-14 16:05:35 +0900

    add parameter to a route

--- image-api/main.go
+++ image-api/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+  "fmt"
   "github.com/gin-gonic/gin"
 )
 
 func router() *gin.Engine {
   r := gin.Default()
-  r.GET("/", func(c *gin.Context) {
-    c.String(200, "Hello")
+  r.GET("/:name", func(c *gin.Context) {
+    user := c.Param("name")
+    c.String(200, fmt.Sprintf("Hello, %s", user))
   })
 
   return r
