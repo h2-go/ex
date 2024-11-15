f60191e8c46871b171e195fe592595f3eaf7788e
Hyunghwan Shin <hwan.shin@gmail.com>
2024-11-15 11:59:01 +0900

    Post, file upload

--- image-api/main.go
+++ image-api/main.go
@@ -2,9 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
+
+type Message struct {
+	Name  string `json:"name"`
+	Email string `json:"email" binding:"required,email"`
+}
 
 func router() *gin.Engine {
 	r := gin.Default()
@@ -12,10 +19,25 @@ func router() *gin.Engine {
 	{
 		userRoute.GET("/hello/:name", func(c *gin.Context) {
 			user := c.Param("name")
-			c.String(200, fmt.Sprintf("Hello, %s", user))
+			response := fmt.Sprintf("Hello, %s", user)
+			c.String(200, response)
+		})
+		userRoute.POST("/hello", func(c *gin.Context) {
+			body := Message{}
+			if err := c.BindJSON(&body); err != nil {
+				c.AbortWithError(http.StatusBadRequest, err)
+				return
+			}
+			fmt.Println(body)
+			c.JSON(http.StatusAccepted, &body)
+		})
+		userRoute.POST("/upload", func(c *gin.Context) {
+			file, _ := c.FormFile("file")
+			log.Println(file.Filename)
+			c.SaveUploadedFile(file, "/tmp/tempfile")
+			c.String(http.StatusOK, fmt.Sprintf("'%s' uploaded!", file.Filename))
 		})
 	}
-
 	return r
 }
 
